6f1c1f89755f541148ab79b63ff57b9b9f448905
RuskyDev <91284372+RuskyDev@users.noreply.github.com>
2023-07-15 20:05:38 +0500

    Bug Fixs

--- src/litely.py
+++ src/litely.py
@@ -1,5 +1,9 @@
 import requests
 import json
+from colorama import init, Fore
+import os
+
+init()
 
 class Litely:
     def __init__(self, current_version):
@@ -15,9 +19,21 @@ class Litely:
             return None
 
     def check_for_updates(self, repo):
-        print("[Litely] Checking for Updates...")
+        print(f"{Fore.WHITE}[{Fore.YELLOW}Litely{Fore.WHITE}] Checking for Updates...")
         latest_release = self.get_latest_release(repo)
         if latest_release and latest_release != self.current_version:
-            print(f"[Litely] Update found! Latest version: {latest_release}")
+            print(f"{Fore.WHITE}[{Fore.YELLOW}Litely{Fore.WHITE}] Update found! Latest version: {latest_release}")
+            self.download_update(repo, latest_release)
         else:
-            print("[Litely] No updates available.")
+            print(f"{Fore.WHITE}[{Fore.YELLOW}Litely{Fore.WHITE}] No updates available.")
+
+    def download_update(self, repo, latest_version):
+        print(f"{Fore.WHITE}[{Fore.YELLOW}Litely{Fore.WHITE}] Downloading update...")
+        url = f"https://github.com/{repo}/raw/{latest_version}/update.bat"
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open("temp/update.bat", "wb") as file:
+                file.write(response.content)
+            print(f"{Fore.WHITE}[{Fore.YELLOW}Litely{Fore.WHITE}] Update downloaded successfully.")
+        else:
+            print(f"{Fore.WHITE}[{Fore.YELLOW}Litely{Fore.WHITE}] Failed to download the update.")
--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 from litely import Litely
 
-current_version = "0.0.3"
+current_version = "0.0.1"
 
 updater = Litely(current_version)
 updater.check_for_updates("RuskyDev/ruskydev.github.io")
